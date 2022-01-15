a5df7785ef2adee9a4d8c07ed69e01334465d8cc
Rolando Jesus Zafra Moran <rolando20022016@hotmail.com>
2022-01-14 23:23:32 -0500

    RQTO index
    mejora del index

--- src/keys.js
+++ src/keys.js
@@ -3,6 +3,6 @@ module.exports = {
         host: `${process.env.MYSQL_HOST}`,
         user: `${process.env.MYSQL_USER}`,
         password: `${process.env.MYSQL_PASSWORD}`,
-        database: `${process.env.MYSQL_DATABASE}_test`
+        database: `${process.env.MYSQL_DATABASE}`
     }
 };
