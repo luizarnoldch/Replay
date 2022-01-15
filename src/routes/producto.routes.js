6420f9929c7f6fb45d92cabb61215d4b458f3c39
Rolando Jesus Zafra Moran <rolando20022016@hotmail.com>
2022-01-15 14:23:48 -0500

    header producto

--- src/routes/producto.routes.js
+++ src/routes/producto.routes.js
@@ -1,8 +1,26 @@
 const {Router} = require("express");
 const router = Router();
+const connection = require("../database.js");
 
 router.get("/:id", (req, res) => {
-    res.render("producto");
+    const productID = req.params.id;
+
+    connection.query(`CALL SP_PRODUCTOS_BUSCAR(${productID});`, async(error, results) => {
+        if(error) console.log(error);
+        else{
+            let product = results[0];
+
+            connection.query(`CALL SP_PRODUCTOS_LISTA_CATEGORIA(${product[0].idcategory});`, async(error, results) => {
+                if(error) console.log(error);
+                else{
+                    let recomend = results[0];
+                    
+                    res.render("producto", {producto: product, recomendados: recomend});
+                }
+            });
+
+        }
+    });
 });
 
 module.exports = router;
