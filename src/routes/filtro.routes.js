702132f4a3598d9771489ca7b37dad3e12aae52a
arnold <luizarnolch@gmai.com>
2022-01-15 12:30:05 -0500

    filtro

--- src/routes/filtro.routes.js
+++ src/routes/filtro.routes.js
@@ -1,6 +1,10 @@
 const {Router} = require("express");
 const router = Router();
 
+router.get("/", (req, res) =>{
+    res.render("filtro");
+});
+
 router.get("/:id", (req, res) =>{
     res.render("filtro");
 });
