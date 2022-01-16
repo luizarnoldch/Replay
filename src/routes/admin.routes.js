f5ea55e45961514e5902556edecf9104a13bbd1a
Rolando Jesus Zafra Moran <rolando20022016@hotmail.com>
2022-01-16 14:23:13 -0500

    admin completo

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -1,8 +1,12 @@
 const {Router} = require("express");
 const router = Router();
 
+router.get("/admin/login", (req, res) => {
+    res.render("admin_auth", {layout: false});
+});
+
 router.get("/admin", (req, res) =>{
-    res.render("carrito");
+    res.render("admin", {layout: false});
 });
 
 module.exports = router;
