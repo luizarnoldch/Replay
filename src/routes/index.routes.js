ff6c681206249d83834728af348269423efb3106
arnold <luizarnolch@gmai.com>
2022-01-19 10:08:48 -0500

    maria

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -3,33 +3,7 @@ const router = Router();
 const connection = require("../database.js");
 
 router.get("/", (req, res) =>{
-    let tech, ofert, home, beuty;
-    /*
-    connection.query(`CALL SP_PRODUCTOS_LISTA_CATEGORIA(6);`, async(error, results) => {
-        if(error) console.log(error);
-        else{
-            tech = results[0];
-            
-            connection.query(`CALL SP_PRODUCTOS_LISTA_CATEGORIA(5);`, async(error, results) => {
-                if(error) console.log(error);
-                else{
-                    home = results[0];
-                    
-                    connection.query(`CALL SP_PRODUCTOS_LISTA_CATEGORIA(7);`, async(error, results) => {
-                        if(error) console.log(error);
-                        else{
-                            beuty = results[0];
-                            
-                            res.render("index", {oferta: null, tecnologia: tech, hogar: home, belleza: beuty});
-                        }
-                    });
-
-                }
-            });
-
-        }
-    });
-    */
+    res.render("index");
 });
 
 module.exports = router;
