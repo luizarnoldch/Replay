7e1f3f3a858659e09bde159289f1fa097a41e3e9
arnold <luizarnolch@gmai.com>
2022-01-19 10:04:48 -0500

    heroku

--- src/keys.js
+++ src/keys.js
@@ -1,8 +1,10 @@
 module.exports = {
     database: {
-        host: `${process.env.MYSQL_HOST}`,
-        user: `${process.env.MYSQL_USER}`,
-        password: `${process.env.MYSQL_PASSWORD}`,
-        database: `${process.env.MYSQL_DATABASE}`
+        host: `us-cdbr-east-05.cleardb.net`,
+        user: `b7fb56ca14ef97`,
+        password: `bb17b0ec`,
+        database: `heroku_4b88be98e060a06`
     }
 };
+
+//mysql://b7fb56ca14ef97:bb17b0ec@us-cdbr-east-05.cleardb.net/heroku_4b88be98e060a06?reconnect=true
